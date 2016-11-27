6b494b776a05d02b221c1632d6728c647f42db00
MaximeVion <maxime.vion@eemi.com>
2016-11-27 15:34:41 +0100

    ajout de gif

--- wp-content/plugins/monplugin/js/my-js.js
+++ wp-content/plugins/monplugin/js/my-js.js
@@ -1,61 +1,76 @@
+
+
+
 // A l'envoie du formulaire
-$("#myform").submit(function(event) {
+jQuery(function ($){
 
-    // Desactiver evenement de base
-    event.preventDefault();
+    $("#monbouton").on('click', (function(event) {
+        // Desactiver evenement de base
+        event.preventDefault();
 
-    // Recuperer la value du champ
-    value = $("#myinput").val();
-
-    // Envoie de la requete
-    $.ajax({
-        //url: 'http://api.giphy.com/v1/gifs/translate?s=' + value + '&api_key=dc6zaTOxFJmzC',
-        url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + value,
-        type: 'GET',
-        dataType: 'json',
-        data: {}
-    })
-    .done(function(json) {
-        // Requete a fonctionner
-        //$("#mon_gif").attr('src' , json.data.images.downsized_large.url );
-        $("#mon_gif").attr('src' , json.data.image_original_url);
-        console.log(json);
-        //console.log(json);
-        console.log("success 1 div");
-    })
-    .fail(function() {
-        // Requete a pas fonctionner
-        console.log("error");
-    })
-    .always(function() {
-        // Peut importe, se lance tout le temps
-        console.log("complete");
-    });
-    // Recuperer la value du champ
-    value = $("#myinput").val();
+        // Recuperer la value du champ
+        value = $("#myinput").val();
 
 
-    // Envoie de la requete
-    $.ajax({
-        url: 'http://api.giphy.com/v1/gifs/translate?s=' + value + '&api_key=dc6zaTOxFJmzC',
-        //url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + value,
-        type: 'GET',
-        dataType: 'json',
-        data: {}
-    })
-    .done(function(json) {
-        // Requete a fonctionner
-        $("#mon_gif1").attr('src' , json.data.images.downsized_large.url );
-        //$("#mon_gif").attr('src' , json.data.image_original_url);
-        console.log(json);
-        console.log("success 2 div");
-    })
-    .fail(function() {
-        // Requete a pas fonctionner
-        console.log("error");
-    })
-    .always(function() {
-        // Peut importe, se lance tout le temps
-        console.log("complete");
-    });
+        // Envoie de la requete
+        $.ajax({
+            //url: 'http://api.giphy.com/v1/gifs/translate?s=' + value + '&api_key=dc6zaTOxFJmzC',
+            url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + value,
+            type: 'GET',
+            dataType: 'json',
+            data: {}
+        })
+        .done(function(json) {
+
+            // La requete a fonctionnée
+            $("#mon_gif").attr('src', json.data.image_original_url);
+            console.log("success 1 div");
+        })
+        .fail(function() {
+
+            console.log("failure");
+
+        })
+        .always(function() { 
+
+            // Peut importe, se lance tout le temps
+            console.log("complete");
+        });
+
+        // Recuperer la value du champ
+        value = $("#myinput").val();
+
+        // Envoie de la requete
+        $.ajax({
+            url: 'http://api.giphy.com/v1/gifs/translate?s=' + value + '&api_key=dc6zaTOxFJmzC',
+            //url: 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=' + value,
+            type: 'GET',
+            dataType: 'json',
+            data: {}
+        })
+
+        .done(function(json) {
+
+            // La requete a fonctionnée
+            $("#mon_gif1").attr('src', json.data.images.downsized_large.url );
+
+            console.log(json);
+            console.log("success 2 div");
+        })
+        .fail(function() {
+
+            // La requete n'a pas fonctionnée
+            console.log("failure1");
+        })
+        .always(function() {
+
+            // Peut importe, se lance tout le temps
+            console.log("complete");
+        });
+}));
 });
+
+
+
+
+
--- wp-content/plugins/monplugin/monplug.php
+++ wp-content/plugins/monplugin/monplug.php
@@ -12,24 +12,36 @@ Author URI : http://localhost:8888/monsitemax/
 defined( 'ABSPATH' )
     or die ( 'Sorry!' );
 
-function ajout_input(){ ?>
+define( 'MON_APPEL', plugin_dir_url( __FILE__ ) );
 
-            <div id='mon_ajout'>
-                <form id='myform' >
-                  <label for='myinput'>Chercher un Gif</label><br />
-                  <input type='text' id='myinput' name='myinput'/>
-                  <button type='submit'>Chercher</button>
-                </form>
-                <div id='mes_gifs'>
-                    <img id='mon_gif' src=''>
-                    <img id='mon_gif1' src=''>
-                </div>
+
+function MV_add_input(){
+
+echo "<div id='mon_ajout'>
+            <form id='myform' >
+                <label for='myinput'>Chercher un Gif</label><br />
+                <input type='text' id='myinput' name='myinput'/>
+                <button id='monbouton' type='submit'>Chercher</button>
+            </form>
+            <div>
+                <img id='mon_gif' src=''>
+                <div id='error_msg'></div>
             </div>
-    <?php
-    wp_enqueue_script( 'my-js', plugin_dir_url('monplugin') . 'monplugin/js/jquery-3.1.1.js', array("jquery"),NULL, false);
-    wp_enqueue_script( 'my-js', plugin_dir_url('monplugin') . 'monplugin/js/my-js.js', array("jquery"),NULL, true );
-    wp_enqueue_style( 'my-css', plugin_dir_url('monplugin') . 'monplugin/css/my-css.css');
+            <div>
+                <img id='mon_gif1' src=''>
+                <div id='error_msg1'></div>
+            </div>
+        </div>";
+
 }
 
-add_action('the_content', 'ajout_input');
- ?>
+add_action('wp_head', 'MV_add_input');
+
+
+function MV_add_script_style() {
+
+    wp_enqueue_script( 'my-js', MON_APPEL . 'js/my-js.js', array("jquery"),NULL, true );
+    wp_enqueue_style( 'my-css', MON_APPEL . 'css/my-css.css');
+}
+
+add_action( 'wp_enqueue_scripts', 'MV_add_script_style' );
